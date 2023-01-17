e07651e8b389c422bcc89f965982ba3ffdea366c
sebahattin <sebkucuk05@gmail.com>
2023-01-17 10:35:10 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react'
+import './App.css'
+import Game from "./components/Game"
+
+function App() {
+
+  return (
+    <div className="App">
+      Burası app.jsx
+      <Game />
+    </div>
+  )
+}
+
+export default App
